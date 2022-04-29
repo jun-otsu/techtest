0432f60d3d7f7b1f7ff64284a9f51f7dec91b8ff
Jun Otsu <work@jun-otsu.com>
2022-04-29 21:05:56 +0900

    書籍登録機能

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
 use App\Book;
+use App\Http\Requests\BookRegisterRequest;
 
 class BookController extends Controller
 {
@@ -25,5 +26,22 @@ class BookController extends Controller
 		$return = json_decode($response->getBody(), true);
 		$book = $return[0]['summary'];
 		return view('search', compact('book'));
+	}
+
+	public function store(BookRegisterRequest $request)
+	{
+		try {
+			$client = new Client();
+			$response = $client->request('GET','https://api.openbd.jp/v1/get?isbn='.$request->isbn);
+		} catch (\GuzzleHttp\Exception\ConnectException $e) {
+			return redirect()->route('book.index')->with('ただいま書籍検索に不具合が生じております。しばらくしてから再度お試しください。');
+		}
+		$return = json_decode($response->getBody(), true);
+		$book = $return[0]['summary'];
+		if (!$book) {
+			return redirect()->route('book.search')->with('error', __('common.message.register_failed'));
+		}
+		Book::create(['isbn' => $book['isbn'], 'title' => $book['title'], 'author' => $book['author'], 'registered_at' => date('Y-m-d H:i:s')]);
+		return redirect()->route('book.index')->with('success', __('common.message.register_success'));
 	}
 }
